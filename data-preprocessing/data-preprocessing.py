bcfad69b45f002c4186f0774a6f3f374658d9018
shubhambaid <shubhambaid99@gmail.com>
2019-09-14 23:48:01 +0530

    finally working !!

--- data-preprocessing/data-preprocessing.py
+++ data-preprocessing/data-preprocessing.py
@@ -6,7 +6,7 @@ X = dataset.iloc[ : , :-1].values
 Y = dataset.iloc[ : , 3].values
 
 from sklearn.impute import SimpleImputer
-imputer = SimpleImputer(missing_values = "NaN" , strategy = "mean")
+imputer = SimpleImputer(missing_values=np.nan , strategy = "mean", verbose = "0")
 imputer = imputer.fit(X[ : , 1:3])
 X[ : , 1:3] = imputer.transform(X[ : , 1:3])
 
