80fc2916a58ef3e056bc1478f4c88e04326262d6
shubhambaid <shubhambaid99@gmail.com>
2019-09-14 22:51:35 +0530

    updated sklearn syntaxes

--- data-preprocessing/data-preprocessing.py
+++ data-preprocessing/data-preprocessing.py
@@ -5,8 +5,8 @@ dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[ : , :-1].values
 Y = dataset.iloc[ : , 3].values
 
-from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values = 'NaN', strategy = "mean", axis = 0)
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values = "NaN" , strategy = "mean")
 imputer = imputer.fit(X[ : , 1:3])
 X[ : , 1:3] = imputer.transform(X[ : , 1:3])
 
@@ -19,7 +19,7 @@ X = onehotencoder.fit_transform(X).toarray()
 labelencoder_Y = LabelEncoder()
 Y =  labelencoder_Y.fit_transform(Y)
 
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split( X , Y , test_size = 0.2, random_state = 0)
 
 from sklearn.preprocessing import StandardScaler
