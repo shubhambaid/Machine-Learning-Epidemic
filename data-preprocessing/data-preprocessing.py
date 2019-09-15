388fc53f76a77bd263b5767bb172046aea117829
shubhambaid <shubhambaid99@gmail.com>
2019-09-15 22:11:22 +0530

    Added Comments

--- data-preprocessing/data-preprocessing.py
+++ data-preprocessing/data-preprocessing.py
@@ -5,23 +5,28 @@ dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[ : , :-1].values
 Y = dataset.iloc[ : , 3].values
 
+#Handling the missing data
 from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values=np.nan , strategy = "mean", verbose = "0")
 imputer = imputer.fit(X[ : , 1:3])
 X[ : , 1:3] = imputer.transform(X[ : , 1:3])
 
+#Encoding data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
 X[ : , 0] = labelencoder_X.fit_transform(X[ : , 0])
 
+#Dummy Variable
 onehotencoder = OneHotEncoder(categorical_features = [0])
 X = onehotencoder.fit_transform(X).toarray()
 labelencoder_Y = LabelEncoder()
 Y =  labelencoder_Y.fit_transform(Y)
 
+#Splitting
 from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split( X , Y , test_size = 0.2, random_state = 0)
 
+#Scaling
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
