17eee85a5a00477b233fe42ee0fbdcf39e54acff
shubhambaid <shubhambaid99@gmail.com>
2019-09-16 23:03:01 +0530

    day 3

---
+++ multiple-linear-regression/multiple-linear-regression.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[ : , :-1].values
+Y = dataset.iloc[ : ,  4 ].values
+
+#encoding
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+ct = ColumnTransformer(
+    [('one_hot_encoder', OneHotEncoder(), [3])],    # The column numbers to be transformed (here is [0] but can be [0, 1, 3])
+    remainder='passthrough'                         # Leave the rest of the columns untouched
+)
+
+X = np.array(ct.fit_transform(X), dtype=np.float)
+
+#avoiding trap
+X = X[: , 1:]
+
+#splitting
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
+
+#fitting multiple linear regression
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, Y_train)
+
+#prediction
+y_pred = regressor.predict(X_test)
+
+
+
